33e0f1e70a1bb14ce8af7372feb705d493ab3778
michaelbel <michael-bel@outlook.com>
2022-03-23 21:41:15 +0300

    Update project

--- app/src/main/java/org/michaelbel/previewseekbar/MainActivity.kt
+++ app/src/main/java/org/michaelbel/previewseekbar/MainActivity.kt
@@ -2,32 +2,41 @@ package org.michaelbel.previewseekbar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
+import android.util.Log
 import java.util.concurrent.TimeUnit
+import org.michaelbel.previewseekbar.databinding.ActivityMainBinding
 
 class MainActivity: AppCompatActivity(R.layout.activity_main) {
 
-    companion object {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.previewSeekBar.run {
+            max = TIME_MILLS
+            onPreviewTextChanged = ::previewTextChanged
+        }
+    }
+
+    private fun previewTextChanged(seekBar: PreviewSeekBar, progress: Int): String {
+        val duration: Long = seekBar.progress.toLong()
+
+        val hours: Long = TimeUnit.MILLISECONDS.toHours(duration)
+        val minutes: Long = TimeUnit.MILLISECONDS.toMinutes(duration) -
+                TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(duration))
+        val seconds: Long = TimeUnit.MILLISECONDS.toSeconds(duration) -
+                TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(duration))
+
+        return String.format("%02d:%02d:%02d", hours, minutes, seconds)
+    }
+
+    private companion object {
         /**
          * 01:23:45
          */
         private const val TIME_MILLS = 5025000
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        previewSeekBar.max = TIME_MILLS
-        previewSeekBar.addOnPreviewTextChangeListener(object: PreviewSeekBar.OnPreviewTextChangeListener {
-            override fun onPreviewTextChanged(seekBar: PreviewSeekBar, progress: Int): String {
-                val duration = seekBar.progress.toLong()
-
-                val hours = TimeUnit.MILLISECONDS.toHours(duration)
-                val minutes = TimeUnit.MILLISECONDS.toMinutes(duration) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(duration))
-                val seconds = TimeUnit.MILLISECONDS.toSeconds(duration) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(duration))
-
-                return String.format("%02d:%02d:%02d", hours, minutes, seconds)
-            }
-        })
     }
 }
--- app/src/main/java/org/michaelbel/previewseekbar/PreviewSeekBar.kt
+++ app/src/main/java/org/michaelbel/previewseekbar/PreviewSeekBar.kt
@@ -3,80 +3,70 @@ package org.michaelbel.previewseekbar
 import android.content.Context
 import android.util.AttributeSet
 import android.view.View
-import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import android.view.ViewGroup
 import android.widget.PopupWindow
 import android.widget.SeekBar
 import androidx.appcompat.widget.AppCompatSeekBar
 import androidx.appcompat.widget.AppCompatTextView
 import kotlin.math.roundToLong
 
-class PreviewSeekBar(context: Context, attrs: AttributeSet): AppCompatSeekBar(context, attrs), SeekBar.OnSeekBarChangeListener {
+class PreviewSeekBar(
+    context: Context,
+    attrs: AttributeSet
+): AppCompatSeekBar(context, attrs), SeekBar.OnSeekBarChangeListener {
 
-    interface OnPreviewTextChangeListener {
-        fun onPreviewTextChanged(seekBar: PreviewSeekBar, progress: Int): String
-    }
+    var onPreviewTextChanged: (seekBar: PreviewSeekBar, progress: Int) -> String = { _, _ -> "" }
 
-    private var previewWidth = 0F
-    private var previewHeight = 0F
-    private var previewMargin = 0F
+    private val previewWidth: Float
+        get() = resources.getDimension(R.dimen.preview_width)
+    private val previewHeight: Float
+        get() = resources.getDimension(R.dimen.preview_height)
+    private val previewMargin: Float
+        get() = resources.getDimension(R.dimen.preview_margin)
 
     private var previewPopup: PopupWindow? = null
     private var previewPopupText: AppCompatTextView? = null
-    private var previewTextChangeListener: OnPreviewTextChangeListener? = null
 
     init {
-        setOnSeekBarChangeListener(this)
-
-        val parent = View.inflate(context, R.layout.preview_seekbar, null)
-
-        previewWidth = resources.getDimension(R.dimen.preview_width)
-        previewHeight = resources.getDimension(R.dimen.preview_height)
-        previewMargin = resources.getDimension(R.dimen.preview_margin)
-
-        previewPopupText = parent.findViewById(R.id.text)
-
-        previewPopup = PopupWindow(parent, WRAP_CONTENT, WRAP_CONTENT, false)
+        val contentView: View = View.inflate(context, R.layout.preview_seekbar, null)
+        previewPopupText = contentView.findViewById(R.id.previewText)
+        previewPopup = PopupWindow(
+            contentView,
+            ViewGroup.LayoutParams.WRAP_CONTENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT,
+            false
+        )
         previewPopup?.animationStyle = R.style.SeekBarPreviewAnimation
+        setOnSeekBarChangeListener(this)
     }
 
     override fun onStartTrackingTouch(seekBar: SeekBar) {
-        showPopup()
-    }
-
-    override fun onStopTrackingTouch(seekBar: SeekBar) {
-        hidePopup()
-    }
-
-    override fun onProgressChanged(seekBar: SeekBar, progress: Int, b: Boolean) {
-        val previewText = previewTextChangeListener?.onPreviewTextChanged(this, getProgress())
-        previewPopupText?.text = previewText ?: progress.toString()
-        updatePopup(seekBar)
-    }
-
-    fun addOnPreviewTextChangeListener(listener: OnPreviewTextChangeListener) {
-        previewTextChangeListener = listener
-    }
-
-    private fun showPopup() {
         if (previewPopup?.isShowing == false) {
             previewPopup?.showAsDropDown(this, getXPosition(this), getYPosition(this))
         }
     }
 
-    private fun hidePopup() {
+    override fun onStopTrackingTouch(seekBar: SeekBar) {
         if (previewPopup?.isShowing == true) {
             previewPopup?.dismiss()
         }
     }
 
-    private fun updatePopup(seekBar: SeekBar) {
-        previewPopup?.update(seekBar, getXPosition(seekBar), getYPosition(seekBar), WRAP_CONTENT, WRAP_CONTENT)
+    override fun onProgressChanged(seekBar: SeekBar, progress: Int, b: Boolean) {
+        previewPopupText?.text = onPreviewTextChanged(this, getProgress())
+        previewPopup?.update(
+            seekBar,
+            getXPosition(seekBar),
+            getYPosition(seekBar),
+            ViewGroup.LayoutParams.WRAP_CONTENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        )
     }
 
     private fun getXPosition(seekBar: SeekBar): Int {
-        val width = seekBar.width.toDouble().roundToLong() - seekBar.paddingLeft - seekBar.paddingRight
-        val seekMax = if (seekBar.max == 0) seekBar.progress else seekBar.max
-        val thumbPos = seekBar.paddingLeft + width * seekBar.progress / if (seekMax == 0) 1 else seekMax
+        val width: Long = seekBar.width.toDouble().roundToLong() - seekBar.paddingLeft - seekBar.paddingRight
+        val seekMax: Int = if (seekBar.max == 0) seekBar.progress else seekBar.max
+        val thumbPos: Long = seekBar.paddingLeft + width * seekBar.progress / if (seekMax == 0) 1 else seekMax
         return thumbPos.toInt() - previewWidth.toInt() / 2
     }
 
